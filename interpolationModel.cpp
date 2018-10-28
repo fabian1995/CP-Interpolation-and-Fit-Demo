9143d2ac7d3bda1443183c6fe9cd165caec99ead
Fabian Hummer <f95.hummer@gmail.com>
2018-10-28 15:39:28 +0100

    Add lagrange interpolation to interpolation model

--- interpolationModel.cpp
+++ interpolationModel.cpp
@@ -2,6 +2,7 @@
 
 #include "interpolationModel.h"
 #include "splineInterpolation.h"
+#include "lagrangeInterpolation.h"
 
 InterpolationModel::InterpolationModel(QString name, int intSteps, PlotDataModel* initialData) {
     this->name = name;
@@ -12,17 +13,20 @@ InterpolationModel::InterpolationModel(QString name, int intSteps, PlotDataModel
     const double T_max = initialData->getXData()[initialData->getXData().size()-1];
     
     SplineInterpolation splineInt(initialData->getXData(), initialData->getYData(), 0, 0);
+    LagrInterpolate lagrInt(initialData->getXData(), initialData->getYData());
     
     QVector<double> xValues(steps);
-    QVector<double> yValues(steps);
+    QVector<double> yValuesSpline(steps);
+    QVector<double> yValuesLagr(steps);
     for (int i = 0; i < steps; i++) {
         xValues[i] = T_min + (T_max-T_min) * (double)(i) / (double)(steps-1);
-        yValues[i] = splineInt.splineInterpolate(xValues[i]);
+        yValuesSpline[i] = splineInt.splineInterpolate(xValues[i]);
+        yValuesLagr[i] = lagrInt.polynomial(xValues[i]);
+        
     }
     
-    this->plotModels.append(new PlotDataModel(xValues, yValues, LINE, QString("Spline"), false));
-    
-    //delete splineInt;*/
+    this->plotModels.append(new PlotDataModel(xValues, yValuesSpline, LINE, QString("Spline"), false));
+    this->plotModels.append(new PlotDataModel(xValues, yValuesLagr, LINE, QString("Lagrange"), false));
 }
 
 QString InterpolationModel::getName() {
